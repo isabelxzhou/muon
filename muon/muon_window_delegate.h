417f31233598542b3ee6140d77610a941c93dbd2
Flytre <flytre7@gmail.com>
2025-11-10 14:52:26 -0500

    simple ui demo

--- muon/muon_window_delegate.h
+++ muon/muon_window_delegate.h
@@ -11,23 +11,16 @@ class MuonWindowDelegate : public CefWindowDelegate
 public:
   MuonWindowDelegate(CefRefPtr<MuonHandler> handler, cef_show_state_t initial_show_state);
   void OnWindowCreated(CefRefPtr<CefWindow> window) override;
-  void OnWindowClosing(CefRefPtr<CefWindow> window) override;
-  void OnWindowDestroyed(CefRefPtr<CefWindow> window) override;
-  void OnWindowActivationChanged(CefRefPtr<CefWindow> window,
-                                 bool active) override;
-  void OnWindowBoundsChanged(CefRefPtr<CefWindow> window,
-                             const CefRect& new_bounds) override;
-  void OnWindowFullscreenTransition(CefRefPtr<CefWindow> window,
-                                    bool is_completed) override;
 
+  void OnWindowDestroyed(CefRefPtr<CefWindow> window) override;
 
   cef_show_state_t GetInitialShowState(CefRefPtr<CefWindow> window) override;
 
 
   bool CanClose(CefRefPtr<CefWindow> window) override;
 
-  bool OnKeyEvent(CefRefPtr<CefWindow> window,
-                  const CefKeyEvent& event) override;
+  // bool OnKeyEvent(CefRefPtr<CefWindow> window,
+  //                 const CefKeyEvent& event) override;
 
 
   cef_runtime_style_t GetWindowRuntimeStyle() override;
